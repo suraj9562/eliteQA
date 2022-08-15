fc4c59d32a333cd064586260a0ccc67158c6d58f
Saurabh Sonde <saurabhsonde111@gamil.com>
2022-08-16 00:05:21 +0530

    added router and right side pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import "./App.css";
 import LandingPage from "./pages/landingPage/LandingPage";
 import Login from "./pages/login/Login";
 import PasswordReset from "./pages/passordReset/PasswordReset";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      {" "}
-      {/* <LandingPage />{" "} */}
-      {/* <Login /> */}
-      <PasswordReset />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/password/reset" element={<PasswordReset />} />
+      </Routes>
+    </BrowserRouter>
+
   );
 }
 
--- src/pages/passordReset/PasswordReset.jsx
+++ src/pages/passordReset/PasswordReset.jsx
@@ -1,7 +1,13 @@
 import Styles from "./../login/login.module.css";
-
 import loginLogo from "./../../assets/icons/loginLogo.svg";
 import Button from "./../../subComponents/button/Button";
+import thumbUp from "../../assets/images/Group_5.svg";
+import avatarOne from "../../assets/images/roundAvatarOne.svg";
+import avatarTwo from "../../assets/images/roundAvatarTwo.svg";
+import avatarThree from "../../assets/images/roundAvatarThree.svg";
+import threeDots from "../../assets/images/threeDots.svg";
+import ellipseOne from "../../assets/images/ellipseOne.svg";
+import ellipseTwo from "../../assets/images/ellipseTwo.svg";
 
 function PasswordReset() {
   return (
@@ -27,7 +33,36 @@ function PasswordReset() {
           </div>
         </div>
       </div>
-      <div className={Styles.right}></div>
+      <div className={Styles.right}>
+        <img src={ellipseOne} alt="thumbs up" className={Styles.ellipseOne} />
+        <img src={ellipseTwo} alt="thumbs up" className={Styles.ellipseTwo} />
+        <div className={Styles.boxOne}>
+          <img src={thumbUp} alt="thumbs up" />
+          <div className={Styles.boxText}>
+            <span className={Styles.textOne}>135</span><br />
+            <span className={Styles.textTwo}>Companies Using ElteQA</span>
+          </div>
+        </div>
+        <div className={Styles.imageContainerOne}>
+          <img src={avatarOne} alt="thumbs up" />
+          <img src={avatarTwo} alt="thumbs up" />
+        </div>
+        <div className={Styles.imageContainerTwo}>
+          <img src={avatarThree} alt="thumbs up" />
+          <div className={Styles.boxTwo}>
+            <div className={Styles.boxText}>
+              <span className={Styles.textOne}>Name here</span><br />
+              <span className={Styles.textTwo}>Hiring is now peice of cake!</span>
+            </div>
+          </div>
+        </div>
+
+        <div className={Styles.subText}>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec arcu ipsum, mollis non erat vitae, vulputate ultrices dui</p>
+          <img src={threeDots} alt="thumbs up" />
+        </div>
+
+      </div>
     </div>
   );
 }
