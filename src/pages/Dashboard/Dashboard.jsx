3ac0e8816195e565b2a45b0ce854b0d7c4ed33f8
suraj9562 <wadikarsuraj02@gmail.com>
2022-08-20 12:20:03 +0530

    done with upper conatiner of sidebar

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,53 @@
 import DashBoardHeader from "../../components/dashBoardHeader/DashBoardHeader";
+import Select from "../../subComponents/select/Select";
 import Styles from "./dashboard.module.css";
 
 function Dashboard() {
   return (
     <div className={Styles.container}>
       <DashBoardHeader />
+      <div className={Styles.content}>
+        <div className={Styles.left}>
+          <div className={Styles.sidebar}>
+            <div className={Styles.upper}>
+              <div className={Styles.title}>Test Status</div>
+              <div className={Styles.options}>
+                <div className={Styles.option}>
+                  <input type="checkbox" value="lsOngoing" id="lsOngoing" />{" "}
+                  <label htmlFor="lsOngoing">Ongoing (12)</label>
+                </div>
+                <div className={Styles.option}>
+                  <input type="checkbox" value="lsCompleted" id="lsCompleted" />{" "}
+                  <label htmlFor="lsCompleted">Completed (13)</label>
+                </div>
+                <div className={Styles.option}>
+                  <input type="checkbox" value="lsDraft" id="lsDraft" />{" "}
+                  <label htmlFor="lsDraft">Draft (3)</label>
+                </div>
+              </div>
+            </div>
+            <div className={Styles.mid}>
+              <Select />
+              <Select />
+            </div>
+            <div className={Styles.lower}>
+              <div className={Styles.title}>Test Type</div>
+              <div className={Styles.options}>
+                <div className={Styles.option}>
+                  <input type="checkbox" value="lsPublic" id="lsPublic" />{" "}
+                  <label htmlFor="lsPublic">Public</label>
+                </div>
+                <div className={Styles.option}>
+                  <input type="checkbox" value="lsPrivate" id="lsPrivate" />{" "}
+                  <label htmlFor="lsPrivate">Private</label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={Styles.calender}></div>
+        </div>
+        <div className={Styles.right}></div>
+      </div>
     </div>
   );
 }
---
+++ src/subComponents/select/Select.jsx
@@ -0,0 +1,32 @@
+import Styles from "./select.module.css";
+
+function Select() {
+  return (
+    <>
+      <label className={Styles.select} htmlFor="select">
+        <select name="Created by" id="">
+          <option value="" disabled="disabled" selected="selected">
+            Created by
+          </option>
+          <option value="#">One</option>
+          <option value="#">Two</option>
+          <option value="#">Three</option>
+          <option value="#">Four</option>
+          <option value="#">Five</option>
+          <option value="#">Six</option>
+          <option value="#">Seven</option>
+        </select>
+        <svg>
+          <use href="#select-arrow-down"></use>
+        </svg>
+      </label>
+      <svg className={Styles.sprites}>
+        <symbol id="select-arrow-down" viewbox="0 0 10 6">
+          <polyline points="1 1 5 5 9 1"></polyline>
+        </symbol>
+      </svg>
+    </>
+  );
+}
+
+export default Select;
