6a62cce5a2c4874cebae15d461b38dacf93575f2
@suraj05 <wadikarsuraj02@gmail.com>
2022-08-27 20:11:39 +0530

    done with second screen

--- src/components/Question/bulkQuestion/BulkQuestion.jsx
+++ src/components/Question/bulkQuestion/BulkQuestion.jsx
@@ -3,8 +3,12 @@ import Styles from "./bulkquestion.module.css";
 
 import uploadBtn from "./../../../assets/icons/uploadBtn.svg";
 import Button from "../../../subComponents/button/Button";
+import { useState } from "react";
 
 function BulkQuestion() {
+  const [file, setFile] = useState("");
+  const [text, setText] = useState("");
+
   return (
     <>
       <QuestionHeader />
@@ -15,12 +19,34 @@ function BulkQuestion() {
             document.getElementById("input").click();
           }}
         >
-          <div className={Styles.btnContainer}>
-            <img src={uploadBtn} alt="" />
-            <span>Upload File or drag and drop here</span>
-          </div>
+          {file === "" ? (
+            <div className={Styles.btnContainer}>
+              <img src={uploadBtn} alt="" />
+              <span>Upload File or drag and drop here</span>
+            </div>
+          ) : (
+            <div className={Styles.selected}>
+              <div className={Styles.text}>{text}</div>
+              <Button
+                data="Confirm"
+                styles={{
+                  background: "#00C49A",
+                  border: "1px solid #00C49A",
+                }}
+              />
+            </div>
+          )}
         </div>
-        <input type="file" style={{ display: "none" }} id="input" />
+        <input
+          type="file"
+          style={{ display: "none" }}
+          id="input"
+          value={file}
+          onChange={(e) => {
+            setText(e.target.files[0].name);
+            setFile(e.target.value);
+          }}
+        />
         <div className={Styles.instructions}>
           <div className={Styles.header}>
             <div className={Styles.title}>
@@ -40,22 +66,32 @@ function BulkQuestion() {
               1. Only .xls and .xlsx formats are supported for bulk upload.
             </div>
             <div className={Styles.listItem}>
-              2. Only the first sheet of the Excel file will be considered for bulk upload.
+              2. Only the first sheet of the Excel file will be considered for
+              bulk upload.
             </div>
             <div className={Styles.listItem}>
-              3. You must create a section in the library and enter the same name in the Section name column.
+              3. You must create a section in the library and enter the same
+              name in the Section name column.
             </div>
             <div className={Styles.listItem}>
-              4. Do not remove or change the header row, however, to add more than 4 options for an Multiple choice, create a column next to Option 4 with the header in the same format
+              4. Do not remove or change the header row, however, to add more
+              than 4 options for an Multiple choice, create a column next to
+              Option 4 with the header in the same format
             </div>
             <div className={Styles.listItem}>
-              5. In the Options field, if a number in an option ends with 0 after the decimal point such as 1.0 and 20.0, then change the format of the field to 'text' in the Excel sheet.
+              5. In the Options field, if a number in an option ends with 0
+              after the decimal point such as 1.0 and 20.0, then change the
+              format of the field to 'text' in the Excel sheet.
             </div>
             <div className={Styles.listItem}>
-              6. Multiple skills or topics should be separated using commas and cannot be named 'dummy'. Use valid names such as Java, Python, etc.
+              6. Multiple skills or topics should be separated using commas and
+              cannot be named 'dummy'. Use valid names such as Java, Python,
+              etc.
             </div>
             <div className={Styles.listItem}>
-              7. If there are multiple correct options to a question, you can add the options in the Correct options column by separating them with 
+              7. If there are multiple correct options to a question, you can
+              add the options in the Correct options column by separating them
+              with
             </div>
           </div>
         </div>
