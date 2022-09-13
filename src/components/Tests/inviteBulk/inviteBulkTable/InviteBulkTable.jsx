4c696a1553c4337c41982643e102912dfe96d709
@suraj05 <wadikarsuraj02@gmail.com>
2022-09-13 20:45:21 +0530

    done with invite bulk table screeen

--- src/components/Tests/inviteBulk/inviteBulkTable/InviteBulkTable.jsx
+++ src/components/Tests/inviteBulk/inviteBulkTable/InviteBulkTable.jsx
@@ -1,11 +1,91 @@
 import TestHeader from "../../testHeader";
 import Styles from "./inviteBulkTable.module.css";
 
+import Select from "../../../../subComponents/select/Select";
+import editIcon from "../../../../assets/icons/editIcon.svg";
+import deleteIcon from "../../../../assets/icons/deleteIcon.svg";
+import DifButton from "../../../Question/button/DifButton";
+
 function InviteBulkTable() {
+  const data = [
+    {
+      CandidateName: "Himanshu Patil",
+      EmailID: "abc@gmail.com",
+    },
+    {
+      CandidateName: "Himanshu Patil",
+      EmailID: "abc@gmail.com",
+    },
+    {
+      CandidateName: "Himanshu Patil",
+      EmailID: "abc@gmail.com",
+    },
+    {
+      CandidateName: "Himanshu Patil",
+      EmailID: "abc@gmail.com",
+    },
+  ];
+
   return (
     <div>
       <TestHeader styles={{ display: "none" }} />
-      <div className={Styles.container}></div>
+      <div className={Styles.tableContainer}>
+        <div className={Styles.headingContainer}>
+          <div className={Styles.heading}>Imported Candidates (34)</div>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "5rem",
+            }}
+          >
+            <DifButton
+              data="Discard"
+              styles={{
+                maxWidth: "25rem",
+                background: "#999999",
+              }}
+              textStyle={{ color: "#FFFFFF" }}
+            />
+            <Select data={"Validity"} />
+            <DifButton
+              data="Sent Invitation"
+              styles={{
+                maxWidth: "25rem",
+                background: "#00C49A",
+              }}
+              textStyle={{ color: "#FFFFFF" }}
+            />
+          </div>
+        </div>
+        <div className={Styles.table}>
+          <table>
+            <tr>
+              <th>Sr. No.</th>
+              <th>CandidateName</th>
+              <th>Email</th>
+              <th>Edit</th>
+            </tr>
+            {data.map((val, key) => {
+              return (
+                <tr key={key}>
+                  <td>{key + 1}</td>
+                  <td>{val.CandidateName}</td>
+                  <td>{val.EmailID}</td>
+                  <td>
+                    <div className={Styles.btnContainer}>
+                      <div className={Styles.icon}>
+                        <img src={editIcon} alt="" />
+                        <span>Edit</span>
+                      </div>
+                    </div>
+                  </td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
